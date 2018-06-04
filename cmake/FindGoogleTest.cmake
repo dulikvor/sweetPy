708edab5e347cf73086c44b7c41060a9db22534e
Unknown <likdudi@gmail.com>
2018-06-04 09:33:14 -0700

    Build fixes.

--- cmake/FindGoogleTest.cmake
+++ cmake/FindGoogleTest.cmake
@@ -1,13 +1,11 @@
-find_path(GOOGLE_TEST_INCLUDE_DIR NAMES benchmark PATHS ${PROJECT_DIR}/Third_Party/include NO_DEFAULT_PATH)
-find_library(GOOGLE_TEST_LIBRARY_DIR NAMES benchmark PATHS ${PROJECT_DIR}/Third_Party/lib NO_DEFAULT_PATH)
+find_path(GOOGLE_TEST_INCLUDE_DIR NAMES gtest/gtest.h PATHS ${pyCppConn_3RD_PARTY_DIR}/include NO_DEFAULT_PATH)
+find_library(GOOGLE_TEST_LIBRARY_DIR NAMES libgtest.a PATHS ${pyCppConn_3RD_PARTY_DIR}/lib NO_DEFAULT_PATH)
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(benchmark REQUIRED_VARS GOOGLE_TEST_INCLUDE_DIR GOOGLE_TEST_LIBRARY_DIR)
+find_package_handle_standard_args(GOOGLE_TEST REQUIRED_VARS GOOGLE_TEST_INCLUDE_DIR)
 
 if(GOOGLE_TEST_FOUND)
-    message(STATUS "$Found Google Test include dir - ${Green}${GOOGLE_TEST_INCLUDE_DIR}{ColourReset}")
-    message(STATUS "$Found Google Test library dir - ${Green}${GOOGLE_TEST_LIBRARY_DIR}{ColourReset}")
+    message(STATUS "Found Google Test include dir - ${Green}${GOOGLE_TEST_INCLUDE_DIR}${ColourReset}")
+    message(STATUS "Found Google Test library dir - ${Green}${GOOGLE_TEST_LIBRARY_DIR}${ColourReset}")
 else()
-
     message(WARNING "${Red}Google Test not found${ColourReset}")
-    message(WARNING "Google Test not found")
 endif()
--- cmake/installThirdParty.cmake
+++ cmake/installThirdParty.cmake
@@ -1,8 +1,7 @@
 include(ExternalProject) #In order to load the ExternalProject module and to add the ExternalProject_Add command
-set (EXTERNAL_PROJECT_DIR ${CMAKE_BINARY_DIR}/Third_Party CACHE PATH "External projects location") #Sets a variable with the installation location of all third party
-set_directory_properties(PROPERTIES EP_PREFIX ${EXTERNAL_PROJECT_DIR}) #Sets the prefix(dir) for all installations commands
+set_directory_properties(PROPERTIES EP_PREFIX ${pyCppConn_3RD_PARTY_DIR}) #Sets the prefix(dir) for all installations commands
 include(InstallCore)
-if(UNIX AND NOT APPLE)
+if((pyCppConn_Test_Support) AND (UNIX AND NOT APPLE))
     include(InstallGoogleTest)
 endif()
 
