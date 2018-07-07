24b224bceb24808ceb3aaa112d8ce7b5b3ec94a6
Unknown <likdudi@gmail.com>
2018-07-07 02:05:33 -0700

    Build system fixes.

--- cmake/installThirdParty.cmake
+++ cmake/installThirdParty.cmake
@@ -1,7 +1,7 @@
 include(ExternalProject) #In order to load the ExternalProject module and to add the ExternalProject_Add command
 set_directory_properties(PROPERTIES EP_PREFIX ${sweetPy_3RD_PARTY_DIR}) #Sets the prefix(dir) for all installations commands
 include(InstallCore)
-if((sweetPy_Test_Support) AND (UNIX AND NOT APPLE))
+if((sweetPy_TEST_SUPPORT) AND (UNIX AND NOT APPLE))
     include(InstallGoogleTest)
 endif()
 
