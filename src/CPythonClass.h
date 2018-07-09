c24629ead8d7f3a036c90be660f2d912cdedd29a
Unknown <likdudi@gmail.com>
2018-07-09 03:27:20 -0700

    Default constructors, constructors validation and travis script update.
    1) If the user didn't supplied any constructor to its CPythonClass object, default constructor will be attmpted to be added.
    2) Adding a constructor is now validated, if an attempt to add an un supported constructor is detected an exception is thrown (the existance of a constructor is validated). private constructors are not supported and the validation will do nothing.
    3) An update to travis script not to send email notifications.

--- src/CPythonClass.h
+++ src/CPythonClass.h
@@ -4,6 +4,7 @@
 #include <type_traits>
 #include <vector>
 #include <memory>
+#include <utility>
 #include <unordered_map>
 #include <Python.h>
 #include <structmember.h>
@@ -23,6 +24,63 @@
 
 namespace sweetPy {
 
+    struct check_constructor
+    {
+        template<typename, typename...>
+        static constexpr bool test(...){return false;}
+
+        template<typename T, typename... Args, typename = decltype(T(std::declval<Args>()...))>
+        static constexpr bool test(int){return true;}
+    };
+
+    template<typename T, typename... Args>
+    struct has_function
+    {
+        static const bool value = check_constructor::test<T, Args...>(0);
+    };
+
+    template<size_t Index>
+    struct argument_detector
+    {
+    public:
+        template<typename T>
+        constexpr operator T() const;
+    };
+
+    template<typename T, size_t... I>
+    struct function_detector_impl{};
+
+    template<typename T, size_t I0, size_t...I>
+    struct function_detector_impl<T, I0, I...>
+    {
+        static const bool value = has_function<T, argument_detector<I0>, argument_detector<I>...>::value ||
+                                  function_detector_impl<T, I...>::value;
+    };
+
+    template<typename T>
+    struct function_detector_impl<T>
+    {
+        static const bool value = has_function<T>::value;
+    };
+
+    template<typename T, size_t ArgumentCount>
+    struct FunctionDetector
+    {
+    public:
+        constexpr static bool Detect()
+        {
+            return detect_impl(std::make_index_sequence<ArgumentCount>());
+        }
+    private:
+        template<size_t... I>
+        constexpr static bool detect_impl(std::index_sequence<I...>)
+        {
+           return function_detector_impl<T, I...>::value;
+        }
+    };
+
+
+
     template<typename T, typename Type = typename std::remove_const<typename std::remove_pointer<
             typename std::remove_reference<T>::type>::type>::type>
     class CPythonClass {
@@ -36,6 +94,8 @@ namespace sweetPy {
             InitMembers();
             InitMethods();
             InitStaticMethods();
+            if(m_type->tp_init == nullptr)
+                AddConstructor<>();
             PyType_Ready(m_type.get());
             CPyModuleContainer::Instance().AddType(CPyModuleContainer::TypeHash<CPythonClassType<Type>>(), m_type.get());
             //Init ref type
@@ -60,9 +120,18 @@ namespace sweetPy {
             CPyModuleContainer::Instance().AddStaticMethod(GenerateMethodId<CPyFuncType>(),
                                                            m_cPythonMemberStaticFunctions.back());
         }
+        template<typename... Args>
+        typename std::enable_if<!std::is_constructible<Type, Args...>::value, void>::type AddConstructor()
+        {
+            //use cases for an exception:
+            // 1) The user is attempting to add an unsupported constructor.
+            // 2) Private constructor are not detected!!!
+            if(FunctionDetector<Type, 10>::Detect())
+                throw CPythonException(PyExc_TypeError, __CORE_SOURCE, "Either specify constructor was not added, or unsupported constructor is attempted to be added.");
+        }
 
         template<typename... Args>
-        void AddConstructor() {
+        typename std::enable_if<std::is_constructible<Type, Args...>::value, void>::type AddConstructor() {
             m_type->tp_init = &CPythonConstructor<Type, Args...>::Wrapper;
         }
 
